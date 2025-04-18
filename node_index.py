eb404c6feb60f01a55619c00edbe701b33feb55c
rupayan <rupayan@insynkstudios.com>
2025-04-18 15:45:54 +0530

    Add FindAllUsers and FindUsersFromNamePartial to node mappings

--- node_index.py
+++ node_index.py
@@ -1,11 +1,15 @@
-from .nodes.balance import FetchBankBalance, TransferBalance
+from .nodes.balance import FetchBankBalance, TransferBalance, FindAllUsers, FindUsersFromNamePartial
 
 NODE_CLASS_MAPPINGS = {
     "FetchBankBalance": FetchBankBalance,
-    "TransferBalance": TransferBalance
+    "TransferBalance": TransferBalance,
+    "FindAllUsers": FindAllUsers,
+    "FindUsersFromNamePartial": FindUsersFromNamePartial
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "FetchBankBalance": "Fetch Bank Balance",
-    "TransferBalance": "Transfer Balance"
+    "TransferBalance": "Transfer Balance",
+    "FindAllUsers": "Find All Users",
+    "FindUsersFromNamePartial": "Find Users From Name Partial"
 }
