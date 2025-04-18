89ec49e6eb3a779281c5eece086ba233c37df2c8
rupayan <rupayan@insynkstudios.com>
2025-04-18 14:26:24 +0530

    Refactor environment variable loading for MongoDB configuration

--- nodes/balance.py
+++ nodes/balance.py
@@ -1,10 +1,8 @@
 import os
 from pymongo import MongoClient
-from dotenv import load_dotenv
 
-load_dotenv()
-MONGO_URI = os.getenv("MONGO_URI")
-MONGO_DB_NAME = os.getenv("MONGO_DB_NAME", "bank_db")
+MONGO_URI = os.environ.get("MONGO_URI")
+MONGO_DB_NAME = os.environ.get("MONGO_DB_NAME")
 
 client = MongoClient(MONGO_URI)
 db = client[MONGO_DB_NAME]
